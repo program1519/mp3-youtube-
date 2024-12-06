f9a77e6b1ea843be4f8cc925dc97c81570079e73
program1519 <109532636+program1519@users.noreply.github.com>
2024-12-06 19:52:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static('public')); // ใช้เพื่อให้เข้าถึงไฟล์ในโฟลเดอร์ public
+app.use(express.static('public')); 
 
-// เส้นทางหลัก
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// เส้นทางสำหรับการแปลง
+
 app.post('/convert', async (req, res) => {
     const { link } = req.body;
 
     try {
         const result = await poji_ytmp3(link);
-        res.json(result); // ส่งข้อมูลกลับในรูปแบบ JSON
+        res.json(result); 
     } catch (error) {
         res.json({ error: `Error occurred: ${error.message}` });
     }
